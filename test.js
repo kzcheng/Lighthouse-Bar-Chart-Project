70dde12c9d1acf08658c0e4a8b52cb0f761c6fd0
kzcheng <kevinzifancheng@gmail.com>
2019-12-29 02:55:36 +0000

    Progress on jQuery! I got some extra progress on jQuery. The website now works and seems to react to clicks.

---
+++ test.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+  $("a").click(function (event) {
+
+    alert("As you can see, the link no longer took you to jquery.com");
+
+    event.preventDefault();
+
+    $("a").addClass("test");
+
+  });
+
+
+
+});
